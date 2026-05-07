563f2dad7f6f99053d427355f6a4874cffd12152
Thomas wilce <sirblastkill@gmail.com>
2026-05-07 17:02:50 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualBasic;
+
+namespace C_Test
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void ExampleFunc()
+        {
+            int Input1 = Int32.Parse(InputA.Text);
+            int Input2 = Int32.Parse(InputB.Text);
+            double ShellCount = (Input1 + Input2);
+            double ShotChance = Math.Round((Input2 / ShellCount) * 100,1);
+            string PopUpCaption = "Result:";
+            string PopUpMessage = "There is a " + ShotChance.ToString() + "% chance for a Live Round";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            MessageBox.Show(PopUpMessage, PopUpCaption, buttons);
+        }
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            
+            if (Char.IsNumber(InputA.Text, 0) & Char.IsNumber(InputB.Text,0))
+            {
+                ExampleFunc();
+            }
+            else
+            {
+                string EpopupMessage = "Please enter a number in BOTH boxes";
+                string EpopupCaption = "Error";
+                MessageBoxButtons ErrorButtons = MessageBoxButtons.OK;
+                MessageBox.Show(EpopupMessage, EpopupCaption, ErrorButtons);
+            }
+        }
+    }
+}
